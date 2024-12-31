d57ff6efa9460b6bb04abb5c3f6d562e265228ce
manhnv319 <manhnv319@gmail.com>
2024-12-31 17:45:59 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+c
 
 function App() {
+  const [counter , setCounter] = useState(1);
+
+  const handleIncrease = () => {
+    // Truyen vao 1 gia tri moi cho counter
+    setCounter(prevState => prevState + 1);
+    setCounter(prevState => prevState + 1);
+    setCounter(prevState => prevState + 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{counter}</h2>
+      <button onClick={handleIncrease}>Increase</button>
     </div>
   );
 }
