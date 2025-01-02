ae7998a12199a53fa7c5fccdf1e2ca1a46cd196b
manhnv193 <manhnv@fit-haui.edu.vn>
2025-01-02 23:06:26 +0700

    demo mounted and unmounted

--- src/App.js
+++ src/App.js
@@ -1,42 +1,13 @@
 import { useState } from "react";
-
+import Content from './Content'
 
 function App() {
+  const [show, setShow] = useState(false)
 
-
-
-  const [job, setJob] = useState('')
-  const [jobs, setJobs] = useState(() => {
-    const jsonJobs = localStorage.getItem('jobs')
-    return jsonJobs ? JSON.parse(jsonJobs) : []
-  })
-
-
-  const handleSubmit = () => {
-    setJobs((prev) => {
-      const newJobs = [...prev, job]
-
-      // Save to local storage
-      const jsonJobs = JSON.stringify(newJobs)
-      localStorage.setItem('jobs', jsonJobs)
-
-      return newJobs
-    })
-    setJob('')
-  }
   return (
-    <div className="App" style={{ padding: 20 }}>
-      <input
-        value={job}
-        onChange={e => setJob(e.target.value)} />
-
-      <button onClick={handleSubmit}>Add</button>
-
-      <ul>
-        {jobs.map((job, index) => (
-          <li key={index}>{job}</li>
-        ))}
-      </ul>
+    <div style={{ padding: 20 }}>
+      <button onClick={() => setShow(!show)}>Toggle</button>
+      {show && <Content />}
     </div>
   );
 }
---
+++ src/Content.js
@@ -0,0 +1,9 @@
+
+
+function Content() {
+    return (
+        <h1>Hi anh em</h1>
+    )
+}
+
+export default Content;
