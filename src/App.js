9baca306fe4d3f92429f532a099901e605caadc2
manhnv319 <manhnv319@gmail.com>
2025-01-02 15:26:08 +0700

    two way binding

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
 import { useState } from "react";
 
+const courses = [
+  { id: 1, name: 'React' },
+  { id: 2, name: 'Angular' },
+  { id: 3, name: 'Vue' },
+  { id: 4, name: 'Svelte' },
+  { id: 5, name: 'Ember' }
+]
+
 
 function App() {
-  const [counter , setCounter] = useState(1);
 
-  const handleIncrease = () => {
-    // Truyen vao 1 gia tri moi cho counter
-    setCounter(prevState => prevState + 1);
-    setCounter(prevState => prevState + 1);
-    setCounter(prevState => prevState + 1);
+  const [checked, setChecked] = useState([])
+
+
+  const handleSubmit = () => {
+    console.log({ids: checked})
   }
 
+  const handleCheck = (id) => {
+    setChecked(prev => {
+      if (prev.includes(id)) {
+        return prev.filter(item => item !== id)
+      } else {
+        return [...prev, id]
+      }
+    })
+  }
+
+
   return (
-    <div className="App" style={{padding: 20}}>
-      <h2>{counter}</h2>
-      <button onClick={handleIncrease}>Increase</button>
+    <div className="App" style={{ padding: 20 }}>
+      {courses.map(course => (
+        <div key={course.id}>
+          <input
+            type="checkbox"
+            checked={checked.includes(course.id)}
+            onChange={() => handleCheck(course.id)}
+          />
+          {course.name}
+        </div>
+      ))}
+
+      <button onClick={handleSubmit}>Register</button>
     </div>
   );
 }
