315fd7b0e38af064a4e7d491a30fe12b82d1173a
manhnv193 <manhnv@fit-haui.edu.vn>
2025-01-03 21:15:39 +0700

    how to use useEffect(callback, [deps]) Gọi callback mỗi khi deps thay đổi.

--- src/Content.js
+++ src/Content.js
@@ -7,30 +7,49 @@ import { useEffect, useState } from 'react';
 // - Chỉ gọi callback 1 lần sau khi component được mount.
 
 
+// 3. useEffect(callback, [deps])
+// - Gọi callback mỗi khi deps thay đổi.
+
+
+const tabs = ['posts', 'comments', 'albums']
+
 
 function Content() {
     const [title, setTitle] = useState('')
     const [posts, setPosts] = useState([])
+    const [type, setType] = useState('posts')
 
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(res => res.json())
-        .then(posts => {
-            setPosts(posts)
-        })
-    }, [])
+
+        fetch(`https://jsonplaceholder.typicode.com/${type}`)
+            .then(res => res.json())
+            .then(posts => {
+                setPosts(posts)
+            })
+    }, [type])
 
 
 
     return (
         <div>
+            {tabs.map(tab => (
+                <button
+                    key={tab}
+                    style={type === tab ? {
+                        color: '#fff',
+                        backgroundColor: '#333'
+                    }: {}}
+                    onClick={() => setType(tab)}>
+                    {tab}
+                </button>
+            ))}
             <input
                 value={title}
                 onChange={e => setTitle(e.target.value)}
             />
             <ul>
                 {posts.map(post => (
-                    <li key={post.id}>{post.title}</li>
+                    <li key={post.id}>{post.title || post.name}</li>
                 ))}
             </ul>
         </div>
